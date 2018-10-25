87112b625c75d41b97705d5dad21b3b23b8a7402
Mathias Arens <mathias.arens@googlemail.com>
2018-10-25 09:40:46 -0700

    Initial Commit

---
+++ cpptest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpptest
+//
+//  Created by Mathias Arens on 10/25/18.
+//  Copyright © 2018 Mathias Arens. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
